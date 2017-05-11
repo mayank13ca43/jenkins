f72dd60681a43a2a22a00f49de3711d2f6b49c1e
mayank13ca43 <mayank.fit2010@gmail.com>
2017-05-11 16:15:16 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,4 +1,17 @@
-date
-whoami
-df
-ls
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                sh 'echo "Hello World"'
+                sh date
+                sh ls
+                sh df
+                sh '''
+                    echo "Multiline shell steps works too"
+                    ls -lah
+                '''
+            }
+        }
+    }
+}
